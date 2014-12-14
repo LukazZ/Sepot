81e017742bdcfc24865824c3d17b7f8db7942703
lukazz <lukaz1010@centrum.cz>
2014-12-14 23:20:45 +0100

    Comments added

--- logger.py
+++ logger.py
@@ -5,14 +5,13 @@ log.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+# Log messages to file
 filelog = logging.FileHandler('sepot.log')
 filelog.setLevel(logging.INFO)
 filelog.setFormatter(formatter)
-
 log.addHandler(filelog)
 
-
-
+# Log messages to console 
 cons = logging.StreamHandler()
 cons.setLevel(logging.INFO)
 log.addHandler(cons)
